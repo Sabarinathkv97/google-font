c85cb734c90f7bf12643fac1426be3dec3ed33a0
Sabarinathkv97 <sabarinathkv97@gmail.com>
2023-11-07 11:48:27 +0530

    google font

---
+++ flutter_application_1/lib/home.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class home extends StatefulWidget {
+  const home({super.key});
+
+  @override
+  State<home> createState() => _homeState();
+}
+
+class _homeState extends State<home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+            child: Text('google font sample',
+                style: GoogleFonts.abel(fontSize: 30))),
+      ),
+    );
+  }
+}
